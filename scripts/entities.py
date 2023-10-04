ea3939268c1520c9b2409bc82084a8d88d55fe20
Linkioli <malachishaffer@protonmail.com>
2023-10-04 14:35:22 -0400

    initial commit

---
+++ scripts/entities.py
@@ -0,0 +1,51 @@
+import pygame
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
+        self.velocity = 1
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self, tilemap, movement):
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+        
+        if movement.magnitude() != 0:
+            movement = movement.normalize()
+
+        frame_movement = movement * self.velocity
+
+        # collisions and movement for the x-axis
+        self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                    self.collisions['right'] = True
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = entity_rect.x
+
+        # collisions and movement for the y-axis
+        self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = entity_rect.y
+
+
+    def render(self, surf):
+        surf.blit(self.game.assets['player'], self.pos)
---
+++ scripts/tilemap.py
@@ -0,0 +1,43 @@
+import pygame
+
+NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+PHYSICS_TILES = {'prop'}
+
+class Tilemap:
+    def __init__(self, game, tile_size=16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+
+        # render test tiles to the screen
+        self.tilemap['5;5'] = {'type': 'prop', 'variant': 0, 'pos': (5, 5)}
+        self.tilemap['5;4'] = {'type': 'prop', 'variant': 0, 'pos': (5, 4)}
+        self.tilemap['5;3'] = {'type': 'prop', 'variant': 0, 'pos': (5, 3)}
+        self.tilemap['5;2'] = {'type': 'prop', 'variant': 0, 'pos': (5, 2)}
+        self.tilemap['5;1'] = {'type': 'prop', 'variant': 0, 'pos': (5, 1)}
+
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+        return rects
+
+    def render(self, surf):
+        for tile in self.offgrid_tiles:
+            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
+
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
