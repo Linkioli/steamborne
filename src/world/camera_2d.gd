6ee4e6455755285464e174a37e860c9619283cf6
Linkioli <malachishaffer@protonmail.com>
2024-09-14 11:18:14 -0400

    got camera working in all 4 directions

--- src/world/camera_2d.gd
+++ src/world/camera_2d.gd
@@ -15,9 +15,14 @@ func _process(delta: float) -> void:
 	global_position += movement * SPEED
 	if movement.y:
 		y_movement_factor += SPEED
-	if y_movement_factor >= 128:
+	if y_movement_factor >= size.y:
 		movement = Vector2.ZERO
 		y_movement_factor = 0
+	if movement.x:
+		x_movement_factor += SPEED
+	if x_movement_factor >= size.x:
+		movement = Vector2.ZERO
+		x_movement_factor = 0
 
 
 func _on_camera_north_trigger_body_entered(body: Node2D) -> void:
@@ -25,7 +30,7 @@ func _on_camera_north_trigger_body_entered(body: Node2D) -> void:
 		movement.y = -1
 
 
-func _on_camera_east_trigger_mouse_entered() -> void:
+func _on_camera_east_trigger_body_entered(body: Node2D) -> void:
 	if not moving:
 		movement.x = -1
 
