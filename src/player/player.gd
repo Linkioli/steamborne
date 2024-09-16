4dac6eb1526b87b01d2703f1fd74f05b52ae0c20
Malachi <malachishaffer@protonmail.com>
2024-09-16 11:25:45 -0400

    finished implimenting player push animations

--- src/player/player.gd
+++ src/player/player.gd
@@ -5,6 +5,8 @@ const SPEED = 100
 var direction: Vector2
 var old_vector: Vector2
 
+var pushing = false
+
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 
@@ -29,19 +31,33 @@ func _physics_process(delta: float) -> void:
 func push():
 	if velocity != Vector2.ZERO:
 		if is_on_wall() and velocity.y == 0:
-			print(randi())
+			pushing = true
 		elif is_on_floor() and velocity.x == 0:
-			print(randi())
+			pushing = true
+		elif is_on_ceiling() and velocity.x == 0:
+			pushing = true
+		else:
+			pushing = false
+	else:
+		pushing = false
 
 
 func update_animation_parameters():
 	if velocity == Vector2.ZERO:
 		animation_tree["parameters/conditions/idle"] = true
 		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/is_pushing"] = false 
 	else:
 		animation_tree["parameters/conditions/idle"] = false
 		animation_tree["parameters/conditions/is_moving"] = true
+		animation_tree["parameters/conditions/is_pushing"] = false 
 	
+	if pushing:
+		animation_tree["parameters/conditions/idle"] = false 
+		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/is_pushing"] = true
+
 	if direction != Vector2.ZERO:
 		animation_tree["parameters/Idle/blend_position"] = direction
 		animation_tree["parameters/Walk/blend_position"] = direction
+		animation_tree["parameters/Push/blend_position"] = direction
