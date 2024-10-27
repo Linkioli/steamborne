b16c7ebe859082b60cffcd70870f2bd130a32c49
Malachi <malachishaffer@protonmail.com>
2024-10-27 18:40:16 -0400

    started reworking block scene

--- src/world/block.gd
+++ src/world/block.gd
@@ -1,83 +1,23 @@
-extends CharacterBody2D 
+extends StaticBody2D
 
-const SPEED = 50
+const TILE_SIZE = 16
 
-@export var up_disabled = false
-@export var down_disabled = false
-@export var left_disabled = false
-@export var right_disabled = false
+var speed = 20 
+var direction = Vector2.ZERO
 
-var movement = Vector2.ZERO
+var moving = false
 
-var up = false
-var down = false
-var left = false
-var right = false
+var grid_pos: Vector2
 
 
-func _ready() -> void:
-	if up_disabled:
-		$Up/CollisionShape2D.disabled = true
-	if down_disabled:
-		$Down/CollisionShape2D.disabled = true
-	if left_disabled:
-		$Left/CollisionShape2D.disabled = true
-	if right_disabled:
-		$Right/CollisionShape2D.disabled = true
-
-
-func _physics_process(delta: float) -> void:
-	if Global.player.pushing:
-		# movement in the positive directions is twice as fast because it was moving half the speed for some reason
-		if up:
-			movement.y = 2
-		elif down:
-			movement.y = -1
-		elif left:
-			movement.x = 2
-		elif right:
-			movement.x = -1
-		else:
-			movement = Vector2.ZERO
-
-	if not Global.player.pushing:
-		movement = Vector2.ZERO
-
-	velocity = movement * SPEED
-	move_and_slide()
-
-
-func _on_up_body_entered(body: Node2D) -> void:
-	up = true
-
-
-func _on_up_body_exited(body: Node2D) -> void:
-	movement = Vector2.ZERO
-	up = false
-
-
-func _on_down_body_entered(body: Node2D) -> void:
-	down = true
-
-
-func _on_down_body_exited(body: Node2D) -> void:
-	movement = Vector2.ZERO
-	down = false
-
-
-func _on_left_body_entered(body: Node2D) -> void:
-	left = true
-
-
-func _on_left_body_exited(body: Node2D) -> void:
-	movement = Vector2.ZERO
-	left = false
-
-
-func _on_right_body_entered(body: Node2D) -> void:
-	right = true
-
-
-func _on_right_body_exited(body: Node2D) -> void:
-	movement = Vector2.ZERO
-	right = false
+func _process(delta: float) -> void:
+	grid_pos = floor(global_position / TILE_SIZE)
+	print(grid_pos)
+	if Input.is_action_just_pressed("block_test_up"):
+		print('test')
+		moving = true
+		direction = Vector2.UP
+	
+	if moving:
+		position += direction * speed * delta
+		
