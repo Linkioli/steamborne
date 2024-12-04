3a59393cd38437b6b3adc655a61fa8c18680e528
Malachi <malachishaffer@protonmail.com>
2024-12-04 14:14:19 -0500

    added experimental mode7 game mode

---
+++ src/experimental/player_plane.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+
+const SPEED = 2
+const ROTATION_SPEED = 2
+
+var rotation_dir: int
+
+
+func _physics_process(delta: float) -> void:
+	var movement_vector = Vector3()
+	if Input.is_action_pressed("up"):
+		movement_vector.z -= 1
+	if Input.is_action_pressed("down"):
+		movement_vector.z += 1
+
+	if Input.is_action_pressed("left"):
+		rotation_dir = -1
+	elif Input.is_action_pressed("right"):
+		rotation_dir = 1
+	else:
+		rotation_dir = 0
+
+	rotation_degrees.y -= rotation_dir * ROTATION_SPEED
+	movement_vector = movement_vector.rotated(Vector3(0, 1, 0), rotation.y)
+	move_and_collide(movement_vector * SPEED * delta)
