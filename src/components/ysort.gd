13bea3687564e26568d19c35ce349812b307b858
Malachi <malachishaffer@protonmail.com>
2025-01-05 13:36:38 -0500

    added ysort as a seperate component

--- src/components/ysort.gd
+++ src/components/ysort.gd
@@ -1,12 +1,15 @@
-extends StaticBody2D
+extends Node2D
+class_name YsortComponent
 
+@export var actor: Node
 
 func _process(delta: float) -> void:
 	y_sort()
 
+
 func y_sort():
 	var player_pos = Global.player.global_position
 	if global_position.y <= player_pos.y:
-		z_index = 0
+		actor.z_index = 0
 	elif global_position.y >= player_pos.y:
-		z_index = 1
+		actor.z_index = 1
