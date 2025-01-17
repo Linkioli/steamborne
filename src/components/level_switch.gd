9d1ee2afe141a1d3afffb3dce71629ab23deef92
Malachi <malachishaffer@protonmail.com>
2025-01-17 14:33:48 -0500

    updated level switching logic

--- src/components/level_switch.gd
+++ src/components/level_switch.gd
@@ -2,7 +2,7 @@ extends Area2D
 class_name LevelSwitchComponent
 
 @export var level: PackedScene
-
+signal level_changed
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_packed(level)
+	level_changed.emit(level)
--- src/world/area_1.gd
+++ src/world/area_1.gd
@@ -4,3 +4,10 @@ extends TileMap
 func _ready() -> void:
 	Global.player = $Player
 	Global.camera = $Camera
+
+func change_level(level):
+	get_tree().change_scene_to_packed(level)
+
+
+func _on_level_switch_component_level_changed(level) -> void:
+	change_level(level)
